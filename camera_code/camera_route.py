2516e92ab5caaf09b7607e19ff27610f9c325c3b
Gaurav Mittal <40105592+GM123d@users.noreply.github.com>
2020-03-08 14:37:55 +0530

    Add files via upload

--- camera_code/camera_route.py
+++ camera_code/camera_route.py
@@ -1,19 +1,104 @@
+import argparse
+import cv2
+from inference import Network
 from flask import Flask, Response
 # Raspberry Pi camera module (requires picamera package, developed by Miguel Grinberg) i.e. camera_pi file
 from camera_pi import Camera
 app = Flask(__name__)
 
+CPU_EXTENSION = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
+
+def get_args():
+    '''
+    Gets the arguments from the command line.
+    '''
+    parser = argparse.ArgumentParser("Run inference on an input video")
+    # -- Create the descriptions for the commands
+    m_desc = "The location of the model XML file"
+    d_desc = "The device name, if not 'CPU'"
+    c_desc = "The color of the bounding boxes to draw; RED, GREEN or BLUE"
+    ct_desc = "The confidence threshold to use with the bounding boxes"
+
+    # -- Add required and optional groups
+    parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+
+    # -- Create the arguments
+    required.add_argument("-m", help=m_desc, required=True)
+    optional.add_argument("-d", help=d_desc, default='CPU')
+    optional.add_argument("-c", help=c_desc, default='BLUE')
+    optional.add_argument("-ct", help=ct_desc, default=0.5)
+    args = parser.parse_args()
+
+    return args
+
+
+def convert_color(color_string):
+    '''
+    Get the BGR value of the desired bounding box color.
+    Defaults to Blue if an invalid color is given.
+    '''
+    colors = {"BLUE": (255,0,0), "GREEN": (0,255,0), "RED": (0,0,255)}
+    out_color = colors.get(color_string)
+    if out_color:
+        return out_color
+    else:
+        return colors['BLUE']
+
+
+def draw_boxes(frame, result, args, width, height):
+    '''
+    Draw bounding boxes onto the frame.
+    '''
+    for box in result[0][0]: # Output shape is 1x1x100x7
+        conf = box[2]
+        if conf >= args.ct:
+            xmin = int(box[3] * width)
+            ymin = int(box[4] * height)
+            xmax = int(box[5] * width)
+            ymax = int(box[6] * height)
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), args.c, 1)
+    return frame
+
+
+def infer_on_video(args,frame):
+    # Convert the args for color and confidence
+    args.c = convert_color(args.c)
+    args.ct = float(args.ct)
+    plugin = Network()
+    plugin.load_model(args.m, args.d, CPU_EXTENSION)
+    net_input_shape = plugin.get_input_shape()
+
+    p_frame = cv2.resize(frame, (net_input_shape[3], net_input_shape[2]))
+    p_frame = p_frame.transpose((2,0,1))
+    p_frame = p_frame.reshape(1, *p_frame.shape)
+    plugin.async_inference(p_frame)
+    if plugin.wait() == 0:
+        result = plugin.extract_output()
+        frame = draw_boxes(frame, result, args, frame.shape[2],frame.shape[1])
+    return frame  
+
+
 def gen(camera):
+    args = get_args()
     #Vedio Streaming generating function
     while True:
         frame = camera.get_frame()
+        frame=infer_on_video(args,frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 @app.route('/video_feed')
-def video_feed():
 
+def video_feed():
+    
     #Vedio straming route put this on your img tag Ex: <img src="ip_adress/vedio_feed"
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port =5000, debug=True, threaded=True)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port =5000, debug=True, threaded=True)    
+
+
+
+   
